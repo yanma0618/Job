765b611c1cae5b96bb1539a3111dca3af36d623a
yanma <yan.ma@fuhu.com>
2015-08-26 11:30:03 -0700

    update:

--- Company/G/RMQ.java
+++ Company/G/RMQ.java
@@ -2,33 +2,35 @@ package G;
 
 public class RMQ {
 	public static void main(String[] args){
-    int[] list={0,1,2,5,4,0,1,6,3};
-    System.out.println(min(list,2,4));
+    int[] list={9,1,2,5,4,6,3};
+    System.out.println(log(7));
+    System.out.println(min(list,0,6));
   }
   
-  static int getLog(int x) {
-    int ret = -1;
-    while (x > 0) {
-      ret++;
-      x >>= 1;
-    }
-    return ret;
+  static int log(int x){
+	  int ret=-1;
+	  while(x>0){
+		  ret++;
+		  x=x>>1;
+	  }
+	  return ret;
   }
   
-  static int min(int[] list, int start, int end){
-    int n = list.length;
-    int h = getLog(n);
-    int[][] helper=new int[n][h + 1];
-    for (int i = 0; i < n; i++) helper[i][1] = list[i];
-    for (int j = 2; j <= h; j++) 
-      for (int i = 0; i < n; i++) {
-        if (i + (1<<j) <= n) {
-          helper[i][j] = Math.min(helper[i][j - 1], helper[i + (1 <<(j - 1))][j - 1]);
-        }
-      }
-    
-    int l = getLog(end - start + 1);
-    return Math.min(helper[start][l], helper[end - l + 1][l]);
-    
+  static int min(int[] num, int start, int end){
+	  int len=log(num.length);
+	  int[][] min = new int[num.length][len+1];
+	  for(int i=0;i<num.length;i++){
+		  min[i][0]=num[i];
+	  }
+	  
+	  for(int j=1;j<=len;j++){
+		  for(int i=0;i<num.length;i++){
+			  if(i+(1<<j)-1<num.length){
+				  min[i][j]=Math.min(min[i][j-1],min[i+(1<<(j-1))][j-1]);
+			  }
+		  }
+	  }
+	  int k=log(end-start+1);
+	  return Math.min(min[start][k],min[end-(1<<k)+1][k]);
   }
 }
