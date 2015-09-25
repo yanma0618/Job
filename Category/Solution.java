798056957710306f0f3be518ee7ace688f617d60
yanma0618 <lavender661188@gmail.com>
2015-09-24 22:08:50 -0700

    update

--- Category/Solution.java
+++ Category/Solution.java
@@ -1,51 +1,56 @@
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Definition for a binary tree node. public class TreeNode { int val; TreeNode
  * left; TreeNode right; TreeNode(int x) { val = x; } }
  */
 public class Solution {
-	public static void main(String[] args) {
-		Solution s = new Solution();
-//		System.out.println(s.isMatch("aa", "*"));
-		System.out.println(s.isMatch("aa","a"));
-		System.out.println(s.isMatch("aa","aa"));
-		System.out.println(s.isMatch("aaa","aa"));
-		System.out.println(s.isMatch("aa", "*"));
-		System.out.println(s.isMatch("aa", "a*"));
-		System.out.println(s.isMatch("ab", "?*"));
-		System.out.println(s.isMatch("aab", "c*a*b"));
-
+	
+	public static void main(String[] args){
+		Solution s= new Solution();
+		String[] words={"aa","aa","aa"};
+		System.out.print(s.findSubstring("aaaaaaaa", words));
 	}
+	
+    public List<Integer> findSubstring(String s, String[] words) {
+    	List<Integer> res = new ArrayList<Integer>();
+        HashMap<String, Integer> map = new HashMap<String,Integer>();
+        for(String w: words){
+        	if(map.containsKey(w)){
+        		map.put(w,map.get(w)+1);
+        	}else{
+        		map.put(w,1);
+        	}
+        }
 
-	public boolean isMatch(String s, String p) {
-		if (s == null || p == null)
-			return false;
-		return dfs(s, p, 0, 0);
+        int start=0;
+        int len=words[0].length();
+        int i=0;
+        HashMap<String,Integer> copy=new HashMap<String,Integer>(map);
+        int totallen=len*words.length;
+        while(start+totallen<=s.length() && i+len<=s.length()){
+        	String w=s.substring(i,i+len);
+        	if(copy.containsKey(w)){
+        		copy.put(w,copy.get(w)-1);
+        		if(copy.get(w)==0){
+        			copy.remove(w);	
+        		}
+        		i+=len;
+        		if(i-start==totallen){
+        			res.add(start);
+        			start++;
+            		i=start;
+            		copy=new HashMap<String,Integer>(map);
+        		}
+        	}else{
+        		start++;
+        		i=start;
+        		copy=new HashMap<String,Integer>(map);
+        	}
 
-	}
-
-	public boolean dfs(String s, String p, int i, int j) {
-		if (i >= s.length() && j >= p.length())
-			return true;
-		if (i >= s.length() || j >= p.length())
-			return false;
-		if (p.charAt(j) != '*') {
-			if (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i)) {
-				return dfs(s, p, i + 1, j + 1);
-			} else {
-				return false;
-			}
-		} else {
-			while (j < p.length() && p.charAt(j) == '*') {
-				j++;
-			}
-			for (int k = i; k <= s.length(); k++) {
-				if (dfs(s, p, k, j)) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+        }
+        return res;
+    }
 }
--- Company/G/Dfs_str_with_star.java
+++ Company/G/Dfs_str_with_star.java
@@ -1,25 +1,24 @@
 package G;
 
 public class Dfs_str_with_star {
-		  public static void main(String[] args) {
-		    print("c**",0,"");
-		  }
-		  
-		  static void print(String str, int index,String cur){
-		    if(index==str.length()){
-		      System.out.println(cur);
-		      return;
-		    }
-		    
-		    char c = str.charAt(index);
-		    if(c=='*'){
-		      print(str,index+1,cur+"j");
-		      print(str,index+1,cur+"i");
-		    }else{
-		      print(str,index+1,cur+String.valueOf(c));
-		    }
-		    
-		  }
-		  
+	public static void main(String[] args) {
+		print("c**", 0, "");
+	}
+
+	static void print(String str, int index, String cur) {
+		if (index == str.length()) {
+			System.out.println(cur);
+			return;
+		}
+
+		char c = str.charAt(index);
+		if (c == '*') {
+			print(str, index + 1, cur + "j");
+			print(str, index + 1, cur + "i");
+		} else {
+			print(str, index + 1, cur + String.valueOf(c));
+		}
+
+	}
 
 }
